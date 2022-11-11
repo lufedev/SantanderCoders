1a671cbcd6e86d154d46a1b86a54fcc6d74e8128
Luiz Felipe <73787707+lufedev@users.noreply.github.com>
2022-11-11 11:56:09 -0300

    Create mainSepararNome.java

---
+++ mainSepararNome.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class mainSepararNome {
+    public static void main(String[] args) {
+        Scanner sc1 = new Scanner(System.in);
+        System.out.print("Digite seu nome: ");
+        String frase = sc1.nextLine();
+        String str;
+        String[] fraseArray = frase.split(" ");
+        for (int i = 0; i < fraseArray.length; i++) {
+
+            str = '!'+ fraseArray[i];
+            System.out.print(str + ' ');
+        }
+    }
+}
