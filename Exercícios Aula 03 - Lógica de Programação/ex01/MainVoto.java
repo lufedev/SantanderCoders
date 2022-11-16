958a8cacc5c64dd7609b4848f222bf3c3a8be3ef
Luiz Felipe <73787707+lufedev@users.noreply.github.com>
2022-11-16 08:04:29 -0300

    Create MainVoto.java

---
+++ Exercícios Aula 03 - Lógica de Programação/ex01/MainVoto.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class MainVoto {
+    public static void main(String[] args) {
+        Scanner sc1 = new Scanner(System.in);
+
+        while (true){
+            System.out.print("Digite sua idade: ");
+            String entrada = sc1.nextLine();
+            Integer idade = Integer.parseInt(entrada);
+
+           if (idade > 0 && idade < 15){
+               System.out.println("Sem direito a votar");
+               break;
+
+           } else if ((idade > 15 && idade < 18) || idade > 69 ){
+               System.out.println("Voto facultativo");
+               break;
+
+           } else if (idade > 17 ) {
+               System.out.println("Voto obrigatório");
+               break;
+
+           } else {
+               System.out.println("Valor inválido tente novamente");
+           }
+        }
+    }
+}
+
+
